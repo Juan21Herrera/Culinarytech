3f9512466d3ad61f2cf50cc0c1ff09477e5c47b6
Pinzon250 <santiagopinzo@gmail.com>
2025-04-10 12:23:21 -0500

    Add a endpoint in recipes.py to search similar recipes by title and search recipes by ingredients

--- backend/app/routes/spoonacular/recipes.py
+++ backend/app/routes/spoonacular/recipes.py
@@ -1,7 +1,7 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Query
 from sqlalchemy.orm import Session
-from app.models.db_recipes import Recipe
-from app.schemas.recipe import RecipeSchema
+from app.models.db_recipes import Recipe, SimilarRecipes
+from app.schemas.recipe import RecipeSchema, SimilarRecipesSchema, RecipesWithSimilarSchema
 from app.db.database import get_db
 from dotenv import load_dotenv
 import os, requests
@@ -21,16 +21,15 @@ router = APIRouter(
 API_KEY = os.getenv("SPOONACULAR_API_KEY")
 BASE_URL = "https://api.spoonacular.com"
 
-# Endpoint to get recipes for SpoonacularAPI | User search by name or ingredient
+#     ------------------      Endpoint to get recipes for SpoonacularAPI | User search by name or ingredient         ------------------
+
 @router.get("/search/{title}", response_model=list[RecipeSchema])
 def get_recipes_by_title(title: str, number: int = 5, db: Session = Depends(get_db)):
 
     # 1. Search the recipe in the database
     local_recipes = db.query(Recipe).filter(Recipe.title.ilike(f"%{title}%")).limit(number).all()
 
-
-
-# 2. If not found in the database, search in the Spoonacular API
+    # 2. If not found in the database, search in the Spoonacular API
 
     response = requests.get(
         f"{BASE_URL}/recipes/complexSearch",
@@ -74,16 +73,132 @@ def get_recipes_by_title(title: str, number: int = 5, db: Session = Depends(get_
 
     all_recipes = {recipe.spoonacular_id: recipe for recipe in local_recipes + saved_recipes}
 
-    return list(all_recipes.values())
+    return list(all_recipes.values())   
 
+#       ------------------      Endpoint to get recipes by ingredient for SpoonacularAPI | User search by ingredient       ------------------
 
+@router.get("/ingredients/")
+def search_recipes_by_ingredients(
+        ingredients: str = Query(..., description="Comma-separated list of ingredients"),
+        number: int = Query(5, description="Number of recipes to return"),        
+    ):
+        url = f"https://api.spoonacular.com/recipes/findByIngredients"
 
+        params = {
+            "ingredients": ingredients,
+            "number": number,
+            "apiKey": API_KEY,
+        }
 
+        response = requests.get(url, params=params)
 
-# Endpoint to get all recipes for SpoonacularAPI | Return all recipes
+        if response.status_code != 200:
+            raise HTTPException(status_code=500,detail="Error fetching data from API")
+        
+        data = response.json()
 
-# Endpoint to get recipes by ingredient for SpoonacularAPI | User search by ingredient
+        if not data:
+             raise HTTPException(status_code=404,detail="Recipe not found")
+        
+        return data
 
-# Endpoint to get similar recipes for SpoonacularAPI | Recommendation recipes by user search (cache)
+#       ------------------       Endpoint to get similar recipes for SpoonacularAPI | Recommendation recipes by user search (cache)       ------------------      
 
-# Endpoint to get random recipes for SpoonacularAPI | Random recipes
+@router.get("/{recipe_id}/similar_recipes", response_model=RecipesWithSimilarSchema)
+def get_similar_recipes(
+        title : str = Query(..., description="Title of the recipe"),
+        number: int = Query(5, description="Number of similar recipes to return"),
+        db: Session = Depends(get_db),
+    ):
+        
+        # 1. Search the recipe in the database
+        recipe = db.query(Recipe).filter(Recipe.title.ilike(f"%{title}%")).first()
+
+        if recipe:
+            similar_recipes = db.query(SimilarRecipes).filter(SimilarRecipes.recipe_id == recipe.id).all()
+
+            if similar_recipes:
+                return RecipesWithSimilarSchema(
+                    spoonacular_id=recipe.spoonacular_id,
+                    title=recipe.title,
+                    image=recipe.image,
+                    ingredients = recipe.ingredients,
+                    similar_recipes=[SimilarRecipesSchema(
+                        similar_spoonacular_id=sr.similar_recipe_id,
+                        title= sr.title,
+                        image=sr.image,
+                ) for sr in similar_recipes]
+            )
+        
+        # 2. If not found in the database, search in the Spoonacular API
+        search_url = f"https://api.spoonacular.com/recipes/complexSearch"
+        search_params = {
+            "query": title,
+            "number": number,
+            "apiKey": API_KEY,
+        }
+
+        search_response = requests.get(search_url, params=search_params)
+
+        if search_response.status_code != 200 or not search_response.json().get("results"):
+            raise HTTPException(status_code=500,detail="Error fetching data from API")
+        
+        # Getting the recipe ID from the first result
+        spoonacular_id = search_response.json()["results"][0]["id"]
+        title = search_response.json()["results"][0]["title"]
+        image = search_response.json()["results"][0]["image"]
+
+        # Verify if the recipe already exists in the database
+        existing_recipe = db.query(Recipe).filter(Recipe.spoonacular_id==spoonacular_id).first()
+        if existing_recipe:
+             recipe = existing_recipe
+        else:
+        # Save in the database if not exists
+            recipe = Recipe(spoonacular_id=spoonacular_id, title=title, image=image, ingredients="")
+            db.add(recipe)
+            db.commit()
+            db.refresh(recipe)
+
+        # Get similar recipes in the Spoonacular API
+        similar_url = f"https://api.spoonacular.com/recipes/{spoonacular_id}/similar"
+        similar_params = {
+            "number": number,
+            "apiKey": API_KEY,
+        }
+        response = requests.get(similar_url, params=similar_params)
+
+        if response.status_code != 200:
+            raise HTTPException(status_code=500,detail="Error fetching data from API")
+        
+        similar_data = response.json()
+        
+        if not similar_data:
+             raise HTTPException(status_code=404,detail="Recipe not found") 
+        
+        # Save similar recipes in the database
+        similar_recipes_list = []
+        for item in similar_data:
+             similar_recipes = SimilarRecipes(
+                recipe_id=recipe.id,
+                similar_recipe_id=item["id"],
+                title=item["title"],
+                image=item["image"],
+            )
+        db.add(similar_recipes)
+        similar_recipes_list.append(SimilarRecipesSchema(
+                 similar_spoonacular_id=item["id"],
+                 title=item["title"],
+                 image=item["image"],
+            ))
+        
+        db.commit()
+        
+        # Return similar recipes
+        return RecipesWithSimilarSchema(
+            spoonacular_id=recipe.spoonacular_id,
+            title=recipe.title,
+            image=recipe.image,
+            ingredients = recipe.ingredients,
+            similar_recipes=similar_recipes_list
+        )
+    #       ------------------      Endpoint to get random recipes for SpoonacularAPI | Random recipes       ------------------      
--- backend/app/schemas/recipe.py
+++ backend/app/schemas/recipe.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, List
 
 class RecipeSchema(BaseModel):
     id: int
@@ -7,9 +7,33 @@ class RecipeSchema(BaseModel):
     image: str
     spoonacular_id: int
     instructions: Optional[str]
-    ingredients: Optional[str]
-    cached: bool
+    ingredients: Optional[List[str]]
+    cached: bool # Indicates if the recipe is cached in the database
     
+
+    class Config:
+        orm_mode = True
+
+class RecipeIngredient(BaseModel):
+    id: int
+    recipe_id: int
+    ingredients: str
+    quantity: Optional[str]
+    unit: Optional[str]
+
+    class Config:
+        orm_mode = True
+
+class SimilarRecipesSchema(BaseModel):
+    similar_spoonacular_id: int
+    title: str
+    image: Optional[str]
+
+    class Config:
+        orm_mode = True
+
+class RecipesWithSimilarSchema(BaseModel):
+    similar_recipes: List[SimilarRecipesSchema] = []
 
     class Config:
         orm_mode = True
